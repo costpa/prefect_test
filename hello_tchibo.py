71e5f9b8efbf21c65c295ece343d3b1c04093dd5
Colin Stopa <33684520+costpa@users.noreply.github.com>
2022-01-14 14:03:28 +0100

    Create hello_tchibo.py

---
+++ hello_tchibo.py
@@ -0,0 +1,17 @@
+import prefect
+from prefect import task, Flow
+
+from prefect.storage import Docker
+from prefect.run_configs import DockerRun
+
+@task
+def say_hello():
+    logger = prefect.context.get("logger")
+    logger.info("Hello from ikeade")
+
+with Flow("hello_tchibo", storage=Docker()) as flow:
+    flow.run_config = DockerRun(labels=["tchibo"])
+    say_hello()
+
+flow.register(project_name="tchibo")
+#flow.run()
