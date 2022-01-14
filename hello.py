b99ad717f5bab8b68e44bbf811175baed3d19d1e
Colin Stopa <33684520+costpa@users.noreply.github.com>
2022-01-14 09:33:12 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+import prefect
+from prefect import task, Flow
+
+from prefect.storage import Docker
+from prefect.run_configs import DockerRun
+
+@task
+def say_hello():
+    logger = prefect.context.get("logger")
+    logger.info("Hello, It's me from the git")
+
+with Flow("flow_from_git", storage=Docker()) as flow:
+    flow.run_config = DockerRun(labels=["intern_dev"])
+    say_hello()
+
+flow.register(project_name="intern")
+#flow.run()
