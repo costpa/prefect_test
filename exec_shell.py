826625f8344ab3d6f472aff0a8a51030f6bb5588
Colin Stopa <33684520+costpa@users.noreply.github.com>
2022-01-17 10:13:15 +0100

    Update exec_shell.py

--- exec_shell.py
+++ exec_shell.py
@@ -5,6 +5,8 @@ import subprocess
 from prefect.storage import Docker
 from prefect.run_configs import DockerRun
 
+from datetime import timedelta, datetime
+from prefect.schedules import IntervalSchedule
 
 @task
 def exec_shell_script():
@@ -12,6 +14,11 @@ def exec_shell_script():
     logger.info("I run a shell script now")
     subprocess.call(['sh', './date_to_text.sh'])
 
+schedule = IntervalSchedule(
+    start_date=datetime.utcnow() + timedelta(seconds=1),
+    interval=timedelta(minutes=1),
+)    
+    
 with Flow("exec_shell", storage=Docker()) as flow:
     #config run for docker and labels
     flow.run_config = DockerRun(labels=["ikea_de"])
