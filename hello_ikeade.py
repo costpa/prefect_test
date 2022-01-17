e77781d0c4d216edd078a8c52ded06acfe796936
Colin Stopa <33684520+costpa@users.noreply.github.com>
2022-01-17 09:17:05 +0100

    Update hello_ikeade.py

--- hello_ikeade.py
+++ hello_ikeade.py
@@ -1,6 +1,9 @@
 import prefect
 from prefect import task, Flow
 
+from datetime import timedelta
+from prefect.schedules import IntervalSchedule
+
 from prefect.storage import Docker
 from prefect.run_configs import DockerRun
 
@@ -9,9 +12,12 @@ def say_hello():
     logger = prefect.context.get("logger")
     logger.info("Hello from ikeade")
 
+#set schedule to every minute
+schedule = IntervalSchedule(interval=timedelta(minutes=1))
+
 with Flow("hello_ikeade", storage=Docker()) as flow:
     flow.run_config = DockerRun(labels=["ikea_de"])
     say_hello()
 
-#flow.register(project_name="ikea_de")
-flow.run()
+flow.register(project_name="ikea_de")
+#flow.run()
